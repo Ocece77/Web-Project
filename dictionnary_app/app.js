d2d8cf56365b477268c36352c48e771e39d76a26
Ocece77 <oceanekasindupro@gmail.com>
2024-02-16 23:25:25 +0100

    unfinished project dictionnary app

---
+++ dictionnary_app/app.js
@@ -0,0 +1,32 @@
+const searchBtn = document.getElementById("searching-btn");
+const definitionDiv = document.getElementById("word-definition");
+const dictionnaryApiUrl ="https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+
+
+searchBtn.addEventListener("click", ()=>{
+  let userInput = document.getElementById("word");
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/`
+   fetch(url+ userInput.value)
+   .then(resp => resp.json())
+   .then(data =>{
+    definitionDiv.innerHTML =`
+    <div class="col">
+          <h2 class="text-main fw-bold mb-3">${data.word}</h2>
+          <p class="text-black-50 fs-6">${data.meanings[0].partOfSpeech}</p>
+          <p class="fs-5">${data.meanings[0].definitions[0]["definition"]}</p>
+          <div class="row">
+            <div class="col-1">
+              <div class="vertical-bar"></div>
+          </div>
+          <div class="col">
+            <p class="fst-italic">${data.meanings[0].definitions[0]["example"]}</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    `
+   } )
+   .catch(()=> console.log("error"))
+   
+})
