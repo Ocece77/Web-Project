582b4a3b8120493d92777eb8462f269d2a7f2b96
Ocece77 <oceanekasindupro@gmail.com>
2024-04-05 17:08:19 +0200

    ajout de la fonctionnalité de mettre un prénom

--- hr_project/app.js
+++ hr_project/app.js
@@ -1,10 +1,77 @@
 
+//Obtenir le prénom de l'utilsateur 
+
+const inputPrenom = document.getElementById("input-prenom");
+const btnPrenom = document.getElementById("btn-prenom");
+const helloMessage = document.getElementById("hello-message");
+const askName = document.getElementById("ask-name");
+const navContainer = document.getElementById("navbar-container");
+const mainContainer = document.getElementById("main-container");
+
+
+let getName = () =>{
+  return helloMessage.innerText =  `Hi ${inputPrenom.value} !`;
+}
+
+
+
+//générer des messages
+
+let messages = [
+{
+  id : 1,
+  text :  "Nous préparons du poulet rôti pour le pot de départ de Mathilde, veuillez ne pas le manger cette fois-ci 😡",
+},
+
+{
+  id : 2,
+  text :  "Notre progression ne cesse de s'accélérer 🚀 De nouvelles fonctionnalités et du contenu exclusif sont désormais disponibles sur Bizneo !",
+},
+
+{
+  id : 3,
+  text :  "L'aventure ne fait que commencer ! 🌟 De plus en plus de fonctionnalités passionnantes et de contenu pertinent vous attendent sur Ocean's Lifes. Rejoignez-nous dans cette belle aventure !"
+},
+
+{
+  id : 4,
+  text : "Les bonnes nouvelles ne cessent de s'accumuler ! 🎈 Explorez les dernières fonctionnalités et les contenus inédits dès aujourd'hui. Nous sommes ravis de partager cette expérience avec vous !"
+},
+
+{
+  id : 5,
+  text :" 🌠 Venez découvrir nos toutes nouvelles fonctionnalités et le contenu exclusif que nous avons préparé pour vous. Ensemble, avançons vers de nouveaux sommets !"
+},
+]
+
+const messageDate  = document.getElementById("message-date");
+const messageText  = document.getElementById("message-text");
+
+let i = 0;
+
+let generateMessage = () =>{
+  messageText.classList.add("fade");
+      if ( i > messages.length - 1){
+        i = 0
+        messageText.innerHTML = (messages[i].text.length) < 110  ? `${messages[i].text}` :  `${messages[i].text.slice(0, 110)}...`;
+      } else {
+        messageText.innerHTML = (messages[i].text.length) < 110  ? `${messages[i].text}` :  `${messages[i].text.slice(0, 110)}...`;
+        i++;
+      }
+      
+    setTimeout(()=>{
+      messageText.classList.remove("fade");
+    },500)
+
+   }
+
+
 
 //génére les cartes 
 
 let horaires = [
   {
-    date: "30 Juin",
+    date: "03 Juillet",
     day :"Journée intensive" ,
     start : "9h30",
     end : "20h30"
@@ -72,7 +139,7 @@ fetch(url)
   .then(resp => resp.json())
   .then(data =>{
     console.log(data)
-    weatherTemp.innerText = `${data.main["temp"]}°c `
+    weatherTemp.innerText = `${Math.round(data.main["temp"])}°c `
     weatherImg.innerHTML =  `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="icon">`
   })
   .catch(()=>{
@@ -84,6 +151,20 @@ fetch(url)
 window.addEventListener("DOMContentLoaded", ()=>{
   getWeather()
   generateCard()
+ setInterval(generateMessage , 5000);
+ const monthFull = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const date = new Date()
+ messageDate.innerText = `${date.getDay()} ${monthFull[date.getMonth()]} ${date.getFullYear()}  `;
+
+ btnPrenom.addEventListener("click", () =>{
+  getName();
+  console.log()
+  askName.classList.add("d-none");
+  navContainer.classList.remove("d-none");
+  mainContainer.classList.remove("d-none");
+
+})
+
 })
 
 
